b4b7eb0841c3f6e8acffc143888fde48b90184e4
owilli04 <127900646+owilli04@users.noreply.github.com>
2024-04-30 12:26:01 -0400

    Delete studyBuddy directory

--- studyBuddy/index.js
+++
@@ -1,7 +0,0 @@
-let fName; 
-
-
-document.getElementById('mySubmit').onclick = function(){
-    fName = document.getElementById('myText').value;
-    document.getElementById("welcomeHeader").textContent = `Welcome ${fName}`; 
-}
