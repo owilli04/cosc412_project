00065eb2fbe4dfce77c21b2daa71cbb037d088d5
Riley Forney <161524134+Rforne@users.noreply.github.com>
2024-05-08 15:22:44 -0400

    Update login.js

--- studyBuddy/login.js
+++ studyBuddy/login.js
@@ -33,7 +33,7 @@ submit.addEventListener("click", function (event) {
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            localStorage.setItem('userID', JSON.stringify(user.uid));
+            localStorage.setItem('userID', JSON.stringify(user));
             window.location.href = "dash.html";
             // ...
         })
