56b13e3b0b3def6d836aa7512e3209dbc60f41c7
addigiffin <39894775+addigiffin@users.noreply.github.com>
2024-05-08 13:30:20 -0400

    Update register.js
    pasted from my VS Code

--- studyBuddy/register.js
+++ studyBuddy/register.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
+// importing necessary functions from SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js"; // needed? not in vid
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
-
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDdeo9U1XVB-uSIIlJh0q2Rfhja_lJXVV8",
-    authDomain: "study-buddy-7c5ef.firebaseapp.com",
-    projectId: "study-buddy-7c5ef",
-    storageBucket: "study-buddy-7c5ef.appspot.com",
-    messagingSenderId: "452982202209",
-    appId: "1:452982202209:web:1d7bdce643e6d0e67c2f20"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDdeo9U1XVB-uSIIlJh0q2Rfhja_lJXVV8",
+  authDomain: "study-buddy-7c5ef.firebaseapp.com",
+  projectId: "study-buddy-7c5ef",
+  storageBucket: "study-buddy-7c5ef.appspot.com",
+  messagingSenderId: "452982202209",
+  appId: "1:452982202209:web:1d7bdce643e6d0e67c2f20"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -24,25 +19,23 @@ const app = initializeApp(firebaseConfig);
 const submit = document.getElementById('submit-register');
 const wrapper = document.querySelector('.wrapper');
 
+// when user tries to register their account
+submit.addEventListener("click", function (event) { 
+  event.preventDefault()
+  const email = document.getElementById('email').value; 
+  const password = document.getElementById('password').value;
 
-submit.addEventListener("click", function(event){ // )?
-    event.preventDefault()
-    const email = document.getElementById('email').nodeValue; // value in vid, not listed in IDE, try next
-    const password = document.getElementById('password').nodeValue;
-  
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        alert("Creating account...")
-        //wrapper.classList.remove('active'); // added from other js file
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert("ERROR "  + errorCode + "\n" + errorMessage)
-        
-      }); 
+  const auth = getAuth(app);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("Creating account...")
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
+
+    });
 })
