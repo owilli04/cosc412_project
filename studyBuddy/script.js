17e6ecb331aad97fea8de95f668ddf1833d7b80d
addigiffin <39894775+addigiffin@users.noreply.github.com>
2024-05-07 18:54:13 -0700

    Update script.js
    used wrong key

--- studyBuddy/script.js
+++ studyBuddy/script.js
@@ -28,14 +28,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/fireba
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCx_xvJgzXGU5tiuiOkP5OVui9FzFRrh0s",
-  authDomain: "studybuddynew.firebaseapp.com",
-  projectId: "studybuddynew",
-  storageBucket: "studybuddynew.appspot.com",
-  messagingSenderId: "263166386439",
-  appId: "1:263166386439:web:a8a89f176ab0fb334d25a2"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDdeo9U1XVB-uSIIlJh0q2Rfhja_lJXVV8",
+    authDomain: "study-buddy-7c5ef.firebaseapp.com",
+    projectId: "study-buddy-7c5ef",
+    storageBucket: "study-buddy-7c5ef.appspot.com",
+    messagingSenderId: "452982202209",
+    appId: "1:452982202209:web:1d7bdce643e6d0e67c2f20"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
