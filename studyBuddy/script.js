86deb003136d250a1699897254c8b198f6158091
addigiffin <39894775+addigiffin@users.noreply.github.com>
2024-05-07 18:48:55 -0700

    Update script.js
    adding firebase configurations (need to do before I set up in FB)

--- studyBuddy/script.js
+++ studyBuddy/script.js
@@ -19,3 +19,23 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 })
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCx_xvJgzXGU5tiuiOkP5OVui9FzFRrh0s",
+  authDomain: "studybuddynew.firebaseapp.com",
+  projectId: "studybuddynew",
+  storageBucket: "studybuddynew.appspot.com",
+  messagingSenderId: "263166386439",
+  appId: "1:263166386439:web:a8a89f176ab0fb334d25a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
