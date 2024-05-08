01a410113651d43d2958ee558cbc6e4eae85f9d6
Riley Forney <161524134+Rforne@users.noreply.github.com>
2024-05-08 14:46:29 -0400

    Login now redirects to dashboard

--- studyBuddy/login.js
+++ studyBuddy/login.js
@@ -33,7 +33,7 @@ submit.addEventListener("click", function (event) {
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            alert("Logged in")
+            window.location.href = "dash.html";
             // ...
         })
         .catch((error) => {
