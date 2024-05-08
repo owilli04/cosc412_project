25f4f2938ad602a7b58e4b3f4b4cdce3901f893e
addigiffin <39894775+addigiffin@users.noreply.github.com>
2024-05-08 09:20:40 -0700

    Update script.js

--- studyBuddy/script.js
+++ studyBuddy/script.js
@@ -5,19 +5,23 @@ const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
+    alert(1) // for testing pls do not remove -Addi
     wrapper.classList.add('active');
 })
 
 
 loginLink.addEventListener('click', ()=> {
+    alert(2)
     wrapper.classList.remove('active');
 })
 
 btnPopup.addEventListener('click', ()=> {
+    alert(3)
     wrapper.classList.add('active-popup');
 })
 
 iconClose.addEventListener('click', ()=> {
+    alert(4)
     wrapper.classList.remove('active-popup');
 })
 
