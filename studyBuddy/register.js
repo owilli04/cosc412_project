0df0f2000d7a0240323f2e012c108cd16fbd774b
addigiffin <39894775+addigiffin@users.noreply.github.com>
2024-05-07 19:17:01 -0700

    Update register.js
    cant figure out how to delete just emptying for now

--- studyBuddy/register.js
+++ studyBuddy/register.js
@@ -1,19 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
-// Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDdeo9U1XVB-uSIIlJh0q2Rfhja_lJXVV8",
-    authDomain: "study-buddy-7c5ef.firebaseapp.com",
-    projectId: "study-buddy-7c5ef",
-    storageBucket: "study-buddy-7c5ef.appspot.com",
-    messagingSenderId: "452982202209",
-    appId: "1:452982202209:web:1d7bdce643e6d0e67c2f20"
-  };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
